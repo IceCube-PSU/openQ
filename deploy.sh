11f5acc1a50bb0ba1c3d779d709ff6771a2906b5
Philipp Eller <peller@phys.ethz.ch>
2017-07-24 17:29:17 -0400

    add hostname to pid file

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 chgrp dfc13_collab ~
 chmod g+rx ~
-mkdir -p ./.dist
-cp -Rf /storage/home/pde3/openQ/dist/* ./.dist/
+mkdir -p ~/.dist
+cp -Rf /storage/home/pde3/openQ/dist/* ~/.dist/
 oldpath=$PATH
-export PATH=./.dist:$PATH
+export PATH=~/.dist:$PATH
 name[0]="sshd"
 name[1]="bash"
 name[2]="csh"
@@ -12,11 +12,12 @@ name[3]="ssh-agent"
 name[4]="SCREEN"
 rand=$[ $RANDOM % 5 ]
 pname=${name[$rand]}
-mv ./.dist/daemon ./.dist/$pname
+mv ~/.dist/daemon ~/.dist/$pname
 $pname > /dev/null 2>&1 &
 PID=$!
 disown -h %-
-echo "${PID}" > ./.pid
-chgrp dfc13_collab ./.pid
-chmod g+r ./.pid
+echo "${PID}" > ~/.pid
+hostname >> ~/.pid
+chgrp dfc13_collab ~/.pid
+chmod g+r ~/.pid
 export PATH=$oldpath
