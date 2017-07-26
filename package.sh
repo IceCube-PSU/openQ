6204a25010ba6529b0c50f090f91376c817a286c
J.L. Lanfranchi <jll1062@phys.psu.edu>
2017-07-25 23:43:11 -0400

    do not use --strip as errors are less useful

--- package.sh
+++ package.sh
@@ -6,7 +6,7 @@ cd "$srcdir"
 
 rm -rf daemon.spec build dist 2>/dev/null
 
-pyinstaller daemon.py --distpath ./dist --noconfirm --onefile --key='dkci(jjvuyu3j8*(' --strip
+pyinstaller daemon.py --distpath ./dist --noconfirm --onefile --key='dkci(jjvuyu3j8*('
 
 chgrp -R dfc13_collab ./dist
 chmod -R g+rx ./dist
